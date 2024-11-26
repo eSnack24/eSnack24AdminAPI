0f2ad8201ab725a90d179c33b645efdc0c071df7
DSL0680 <ekthf988172@gmail.com>
2024-11-26 19:05:59 +0900

    feat: graph folder add / merge

---
+++ src/main/java/org/esnack24api/esnack24adminapi/user/dto/UserAddressDTO.java
@@ -0,0 +1,12 @@
+package org.esnack24api.esnack24adminapi.user.dto;
+
+import lombok.Data;
+
+@Data
+public class UserAddressDTO {
+
+    private Long adno;
+    private Long uno;
+    private String country;
+
+}
