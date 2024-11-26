02830e4e1e8a89de17eacbe1c8a8af1fd0f058cd
LeeDongEon <eon@LeeDongEonui-MacBookPro.local>
2024-11-26 15:43:04 +0900

    feat: common - pageRequest

--- src/main/java/org/esnack24api/esnack24adminapi/member/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package org.esnack24api.esnack24adminapi.member.repository;
-
-import org.hyeong.api1014.member.domain.MemberEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<MemberEntity, String> {
-
-
-}
