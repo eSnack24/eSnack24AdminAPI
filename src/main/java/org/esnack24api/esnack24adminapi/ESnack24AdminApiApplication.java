a7caf0cf80d1ef7fa92336cbf8367a0b44eda221
jhyeong <hyeongcode@gmail.com>
2024-11-26 13:04:31 +0900

    Initial commit

---
+++ src/main/java/org/esnack24api/esnack24adminapi/ESnack24AdminApiApplication.java
@@ -0,0 +1,13 @@
+package org.esnack24api.esnack24adminapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ESnack24AdminApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ESnack24AdminApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/esnack24api/esnack24adminapi/ESnack24AdminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.esnack24api.esnack24adminapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ESnack24AdminApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
