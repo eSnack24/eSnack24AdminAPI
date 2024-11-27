556e6bb371a13569b16b9f47d14aa7f71a36f0a1
yun9699 <cha23215@daum.net>
2024-11-27 14:58:56 +0900

    refactor: Allery Impl EntityManger fixed

--- src/main/java/org/esnack24api/esnack24adminapi/allergy/repository/search/AllergySearchImpl.java
+++ src/main/java/org/esnack24api/esnack24adminapi/allergy/repository/search/AllergySearchImpl.java
@@ -2,6 +2,7 @@ package org.esnack24api.esnack24adminapi.allergy.repository.search;
 
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.esnack24api.esnack24adminapi.allergy.domain.AllergyEntity;
 import org.esnack24api.esnack24adminapi.allergy.domain.QAllergyEntity;
 import org.esnack24api.esnack24adminapi.allergy.dto.AllergyCrudDTO;
@@ -13,9 +14,9 @@ public class AllergySearchImpl extends QuerydslRepositorySupport implements Alle
 
     private final JPAQueryFactory queryFactory;
 
-    public AllergySearchImpl(JPAQueryFactory queryFactory) {
+    public AllergySearchImpl(EntityManager entityManager) {
         super(AllergyEntity.class);
-        this.queryFactory = queryFactory;
+        this.queryFactory = new JPAQueryFactory(entityManager);
     }
 
     @Override
