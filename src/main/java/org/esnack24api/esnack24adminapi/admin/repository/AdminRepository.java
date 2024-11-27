3db10f379ceecb35f9d65a3d38393a6b6cdb6375
jhyeong <hyeongcode@gmail.com>
2024-11-27 12:58:22 +0900

    clore: before pull

--- src/main/java/org/esnack24api/esnack24adminapi/admin/repository/AdminRepository.java
+++ src/main/java/org/esnack24api/esnack24adminapi/admin/repository/AdminRepository.java
@@ -1,12 +1,27 @@
 package org.esnack24api.esnack24adminapi.admin.repository;
 
 import org.esnack24api.esnack24adminapi.admin.domain.AdminEntity;
+import org.esnack24api.esnack24adminapi.admin.dto.AdminReadDTO;
 import org.esnack24api.esnack24adminapi.admin.repository.search.AdminSearch;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface AdminRepository extends JpaRepository<AdminEntity, Long>, AdminSearch {
 
     Optional<AdminEntity> findByAdmid(String admid);
+
+    @Query("""
+        select
+           new org.esnack24api.esnack24adminapi.admin.dto.AdminReadDTO(
+           a.admid, a.admname, a.admrole, a.admregdate, a.admmoddate
+           )
+        from
+            AdminEntity a
+        where
+            a.admno = :admno
+    """)
+    AdminReadDTO getAdmin(@Param("admno") Long admno);
 }
--- src/main/java/org/esnack24api/esnack24adminapi/admin/repository/search/AdminSearch.java
+++ src/main/java/org/esnack24api/esnack24adminapi/admin/repository/search/AdminSearch.java
@@ -1,11 +1,9 @@
 package org.esnack24api.esnack24adminapi.admin.repository.search;
 
 import org.esnack24api.esnack24adminapi.admin.dto.AdminListDTO;
-import org.esnack24api.esnack24adminapi.admin.dto.AdminReadDTO;
+import org.esnack24api.esnack24adminapi.admin.dto.AdminWorkListDTO;
 import org.esnack24api.esnack24adminapi.common.dto.PageRequestDTO;
 import org.esnack24api.esnack24adminapi.common.dto.PageResponseDTO;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface AdminSearch {
 
@@ -13,15 +11,5 @@ public interface AdminSearch {
 
     PageResponseDTO<AdminListDTO> adminListByRole(String role, PageRequestDTO pageRequestDTO);
 
-    @Query("""
-        select
-           new org.esnack24api.esnack24adminapi.admin.dto.AdminReadDTO(
-           a.admid, a.admname, a.admrole, a.admregdate, a.admmoddate
-           )
-        from
-            AdminEntity a
-        where
-            a.admno = :admno
-    """)
-    AdminReadDTO getAdmin(@Param("admno") Long admno);
+    PageResponseDTO<AdminWorkListDTO> adminWorkList(PageRequestDTO pageRequestDTO);
 }
